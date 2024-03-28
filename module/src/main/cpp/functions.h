3ab0006d09ee3a53638bb8ec748c39c1ebf3d261
AndreyZinovyevProgramm <93820127+AndreyZinovyevProgramm@users.noreply.github.com>
2024-03-28 13:33:44 +0300

    Update functions.h

--- module/src/main/cpp/functions.h
+++ module/src/main/cpp/functions.h
@@ -2,65 +2,29 @@
 #define ZYCHEATS_SGUYS_FUNCTIONS_H
 
 // here you can define variables for the patches
-bool addCurrency, freeItems, everythingUnlocked, showAllItems, addSkins;
+bool ;
 
 monoString *CreateIl2cppString(const char *str) {
     monoString *(*String_CreateString)(void *instance, const char *str) = (monoString*(*)(void*, const char*)) (g_il2cppBaseMap.startAddress + string2Offset(OBFUSCATE("0x2596B20")));
     return String_CreateString(NULL, str);
 }
 
-void (*PurchaseRealMoney) (void* instance, monoString* itemId, monoString* receipt, void* callback);
+
 
 void Pointers() {
-    PurchaseRealMoney = (void(*)(void*, monoString*, monoString*, void*)) (g_il2cppBaseMap.startAddress + string2Offset(OBFUSCATE("0xE7AADC")));
+
 }
 
 void Patches() {
-    PATCH_SWITCH("0x10A69A0", "200080D2C0035FD6", showAllItems);
-    PATCH_SWITCH("0xF148A4", "E07C80D2C0035FD6", freeItems);
-    PATCH_SWITCH("0x28", "6400A0E31EFF2FE1", addCurrency);
+
 }
 
 // declare your hooks here
-void (*old_Backend)(void *instance);
-void Backend(void *instance) {
-    if (instance != NULL) {
-        if (addCurrency) {
-            LOGW("Calling Purchase");
-            PurchaseRealMoney(instance, CreateIl2cppString("special_offer1"), CreateIl2cppString("dev"), NULL);
-            addCurrency = false;
-            return 1500;
-        }
-        if (addSkins) {
-            LOGW("Calling Skins");
-            addSkins = false;
-        }
-    }
-    return old_Backend(instance);
-}
 
-void* (*old_ProductDefinition)(void *instance, monoString* id, monoString* storeSpecificId, int type, bool enabled, void* payouts);
-void* ProductDefinition(void *instance, monoString* id, monoString* storeSpecificId, int type, bool enabled, void* payouts) {
-    if (instance != NULL) {
-        LOGW("Called ProductDefinition! Here are the parameters:");
-        LOGW("id: %s", id->getChars());
-        LOGW("storeSpecificId: %s", storeSpecificId->getChars());
-        LOGW("type: %i", type);
-    }
-    return old_ProductDefinition(instance, id, storeSpecificId, type, enabled, payouts);
-}
+
 
 //the void *instance is a self-created variable.
-int (*old_Kills)(void *instance);
-int Kills(void *instance) {
-    //Check if instance is NULL to prevent CRASH
-    if (instance != NULL)
-    {
-        return 99999; //Return how many value
-    }
-    //return the original value (this code isn't really needed if you have a toggle/switch)
-    return old_Kills(instance);
-}
+
 
 
 
@@ -68,7 +32,7 @@ int Kills(void *instance) {
 void Hooks() {
     HOOK("0xE7BC74", Backend, old_Backend);
     HOOK("0x29DA08C", ProductDefinition, old_ProductDefinition);
-    HOOK("0x18", Backend, old_Backend);
+
 }
 
 #endif //ZYCHEATS_SGUYS_FUNCTIONS_H
